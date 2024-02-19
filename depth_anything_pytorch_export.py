94484a94abfd988944def8edc77f2832d0018714
nnn <27625496+nnn112358@users.noreply.github.com>
2024-02-19 09:49:34 +0900

    Update depth_anything_pytorch_export.py

--- depth_anything_pytorch_export.py
+++ depth_anything_pytorch_export.py
@@ -38,54 +38,6 @@ traced_script_module = torch.jit.trace(depth_anything, dummy_input, strict=True)
 traced_script_module_file="depth_anything_vits14.pt"
 traced_script_module.save(traced_script_module_file)
 
-"""
-import ncnn  # noqa
-# Export the PyTorch model to ONNX format
-#torch.onnx.export(depth_anything, dummy_input, onnx_path, opset_version=11, input_names=["input"], output_names=["output"], verbose=True)
-    
-print(f"Model exported to ")
-print(f'\n starting export with ncnn {ncnn.__version__}...')
-
-#opt_model = pnnx.export(depth_anything, "depth_anything_vits_14_trial", dummy_input)
-
-
-# 半精度浮動小数点数（FP16）を使用するかどうか（1は使用、0は不使用）
-fp16_data_type = 0
-
-# グラフ最適化レベル（0, 1, 2のいずれか）
-opt_level = 2
-
-# PNNXに渡す追加の引数
-pnnx_args = [
-    'pnnxparam=model.pnnx.param',
-    'pnnxbin=model.pnnx.bin',
-    'pnnxpy=model_pnnx.py',
-    'pnnxonnx=model.pnnx.onnx',
-]
-
-# コマンドラインコマンドの構築
-cmd = [
-    "pnnx",
-    traced_script_module_file,
-    *pnnx_args,
-    f"optlevel={opt_level}",
-    f"fp16={fp16_data_type}",
-#    f"device={'cpu'}",
-    f'inputshape={[1,3,518,518]}',  # モデル入力形状を動的に設定
-]
-
-# コマンドの実行と結果の出力
-print(f"Running: {' '.join(cmd)}")
-ret = subprocess.run(cmd, check=True, capture_output=True, text=True)
-print('Return code:', ret.returncode)
-print('stdout:', ret.stdout)
-
-# 不要なデバッグファイルの削除
-pnnx_files = [arg.split('=')[-1] for arg in pnnx_args]
-for f_debug in ["debug.bin", "debug.param", "debug2.bin", "debug2.param", *pnnx_files]:
-    file_path = Path(f_debug)
-    if file_path.exists():
-        file_path.unlink()
         print(f'Removed: {file_path}')
         
 """
